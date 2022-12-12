5082cf0396b4b698e3182d6ff4a0ac23560b6a95
Avra Mitra <abhra0897@gmail.com>
2022-12-12 16:51:26 +0530

    changed image resolutions for faster loading. More optimizations to do

--- static/virtual_gallery/gallery.js
+++ static/virtual_gallery/gallery.js
@@ -317,7 +317,7 @@ function createRoom(posX, posZ, width, length, height) {
     ceiling.position.y = height;
 
     // Create 4 walls
-    const tfWall = 2;   // Wall texture tiling factor
+    const tfWall = 1.2;   // Wall texture tiling factor
     // Create the base wall
     // NOTE: use async loader if any error is observed. See: https://discourse.threejs.org/t/cloning-texture/14969/2
     const textureWall = new THREE.TextureLoader().load('Bricks019_2K-JPG/Bricks019_2K_Color.jpg');
@@ -403,6 +403,41 @@ function createRoom(posX, posZ, width, length, height) {
     scene.add(groupRoomElements);
 
     return groupRoomElements;
+}
+
+function createDoorFromModel() {
+    // Instantiate a loader
+    const loader = new GLTFLoader();
+
+    // Load a glTF resource
+    loader.load(
+        // resource URL
+        'models/gltf/duck/duck.gltf',
+        // called when the resource is loaded
+        function ( gltf ) {
+
+            scene.add( gltf.scene );
+
+            gltf.animations; // Array<THREE.AnimationClip>
+            gltf.scene; // THREE.Group
+            gltf.scenes; // Array<THREE.Group>
+            gltf.cameras; // Array<THREE.Camera>
+            gltf.asset; // Object
+
+        },
+        // called while loading is progressing
+        function ( xhr ) {
+
+            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+        },
+        // called when loading has errors
+        function ( error ) {
+
+            console.log( 'An error happened' );
+
+        }
+    );
 }
 
 
